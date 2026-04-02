698d4c536761dd307e342c9d0119a0e61dca22a9
irah435 <154801015+irah435@users.noreply.github.com>
2026-04-02 15:21:44 +0800

    Add files via upload

--- FunctionTemplate.cpp
+++ FunctionTemplate.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//Template prefix defines T as a generic datatype 
+// Template prefix defines T as a generic datatype
 template<class T>
-T sum(T a,T b)
+T findMin(T a, T b)
 {
-	T result;
-	result = a + b;
-	return result;
+	if (a < b) {
+		return a; 
+	}
+	else {
+		return b;
+	}
 }
 
-int main() {
-	// The compiler automatically determine the type for T 
-	cout << "Answer integer: " << sum(2, 3) << endl; // T become int 
-	cout << "Answer double: " << sum(1.2, 1.3) << endl; // T become double 
+int main()
+{
+	// The compiler automatically determines the type for T
+	cout << "Smaller value integer: " << findMin(2, 3) << endl; // T becomes int
+	cout << "Smaller value char: " << findMin('H','Z') << endl; // T becomes double
 	return 0;
+
 }
